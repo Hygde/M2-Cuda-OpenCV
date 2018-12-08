0ba05c9c56385a884e298ab795a9b7070693c173
Hygde <samuel.devals@outlook.fr>
2018-12-08 11:50:00 +0100

    massive cleanup

--- simpleTexture.cu
+++ simpleTexture.cu
@@ -10,7 +10,6 @@ todo:
 #include <stdio.h>
 #include <time.h>
 #include <string.h>
-#include <math.h>
 
 // Includes CUDA
 #include <cuda_runtime.h>
@@ -20,21 +19,12 @@ todo:
 
 //#include opencv
 #include <opencv2/opencv.hpp>
-//#include <opencv2/gpu/gpu.hpp>
 
 // Utilities and timing functions
 #include <helper_functions.h>    // includes cuda.h and cuda_runtime_api.h
-
 // CUDA helper functions
 #include <helper_cuda.h>         // helper functions for CUDA error check
 
-#define MAX_EPSILON_ERROR 5e-3f
-
-// Define the files that are to be save and the reference images for validation
-const char *imageFilename = "lena_bw.pgm";
-const char *refFilename   = "ref_rotated.pgm";
-
-const char *sampleName = "simpleTexture";
 
 ////////////////////////////////////////////////////////////////////////////////
 // Texture reference for 2D float texture
@@ -115,9 +105,9 @@ __device__ void dispersionFilter(float *outputData,char*commutation_array, int w
     }else outputData[id] = tex2D(tex, u,v);
 }
 
-__global__ void applyFilters(float *input, float *outputData, char*commutation_array, int width, int height){
+__global__ void applyFilters(unsigned char *input, unsigned char *outputData, char*commutation_array, int width, int height){
     dispersionFilter(input,commutation_array, width, height);
-    __syncthreads();
+    /*__syncthreads();
     medianFilter(input, outputData, width, height);
     __syncthreads();
     sobelFilter(outputData, input, width, height);//result of sobel in input
@@ -125,131 +115,38 @@ __global__ void applyFilters(float *input, float *outputData, char*commutation_a
     multiply(input, outputData, width, height);//multiply result of sobel with result of median*/
 }
 
-////////////////////////////////////////////////////////////////////////////////
-// Declaration, forward
-void runTest(int argc, char **argv);
+void getCommutationArray(char* arr, int size, unsigned int window){for(int i = 0; i < size; i++)arr[i] = (char)rand()%window - (window/2);}
+
+void sendDataToGpu(unsigned char* img, unsigned char*devImg){
+    
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 // Program main
 ////////////////////////////////////////////////////////////////////////////////
 int main(int argc, char **argv){
-    printf("%s starting...\n", sampleName);
+    findCudaDevice(argc, (const char **) argv);
     srand(time(NULL));
-    // Process command-line arguments
-    if (argc > 1)
-    {
-        if (checkCmdLineFlag(argc, (const char **) argv, "input"))
-        {
-            getCmdLineArgumentString(argc,(const char **) argv,"input",(char **) &imageFilename);
+    bool continuer = true;
+    int width = 0, height = 0, window = 3;
+    cv::VideoCapture cam(0); cv::Mat img;
+    unsigned char*data = NULL, *devDat = NULL;
+    cam >> img; width = img.cols; height = img.rows; data = (unsigned char*)img.data;//getting size of the frame
+    char*com = (char*)malloc((width*height+1)*sizeof(char));
 
-            if (checkCmdLineFlag(argc, (const char **) argv, "reference"))getCmdLineArgumentString(argc,(const char **) argv,"reference",(char **) &refFilename);
-            else{
-                printf("-input flag should be used with -reference flag");
-                exit(EXIT_FAILURE);
-            }
-        }
-        else if (checkCmdLineFlag(argc, (const char **) argv, "reference")){
-            printf("-reference flag should be used with -input flag");
-            exit(EXIT_FAILURE);
-        }
-    }
+    //while(continuer){
+        getCommutationArray(com, width*height, window);
+        cam >> img; width = img.cols; height = img.rows; data = (unsigned char*)img.data;
+        printf("%d %d %d\n" , img.at<cv::Vec3b>(0,0).val[0], img.at<cv::Vec3b>(0,0).val[1], img.at<cv::Vec3b>(0,0).val[2]);
+        printf("%d %d %d\n\n" , data[0], data[1], data[2]);
+        if(cv::waitKey(33) == ' ')continuer = false;
+    //}
 
-    runTest(argc, argv);
-
-    printf("%s completed, returned %s\n",sampleName,testResult ? "OK" : "ERROR!");
-    exit(testResult ? EXIT_SUCCESS : EXIT_FAILURE);
+    cam.release();
+    free(com);
+    return 0;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 //! Run a simple test for CUDA
 ////////////////////////////////////////////////////////////////////////////////
-void runTest(int argc, char **argv){
-    int devID = findCudaDevice(argc, (const char **) argv);
-
-    // load image from disk
-    float *hData = NULL;
-    unsigned int width, height;
-    char *imagePath = sdkFindFilePath(imageFilename, argv[0]);
-
-    if (imagePath == NULL){
-        printf("Unable to source image file: %s\n", imageFilename);
-        exit(EXIT_FAILURE);
-    }
-
-    sdkLoadPGM(imagePath, &hData, &width, &height);
-
-    unsigned int size = width * height * sizeof(float);
-    printf("Loaded '%s', %d x %d pixels\n", imageFilename, width, height);
-    dim3 dimBlock(8, 8, 1);
-    dim3 dimGrid(width / dimBlock.x, height / dimBlock.y, 1);
-
-    //Load reference image from image (output)
-    float *hDataRef = (float *) malloc(size);
-    char *refPath = sdkFindFilePath(refFilename, argv[0]);
-
-    char *rindex = (char*) malloc((1+width*height)*sizeof(char));
-    for(int i = 0; i < width*height+1; i++){rindex[i] = rand()%3 - 1;}
-
-    // Allocate device memory for result
-    char *commutation_array;
-    checkCudaErrors(cudaMalloc((void**) &commutation_array, (1+width*height)*sizeof(char)));
-    checkCudaErrors(cudaMemcpy(commutation_array, rindex, (1+width*height)*sizeof(char),cudaMemcpyHostToDevice));
-
-    float *input_1 = NULL, *output = NULL;
-    checkCudaErrors(cudaMalloc((void **) &input_1, size));
-    checkCudaErrors(cudaMalloc((void **) &output, size));
-
-    // Allocate array and copy image data
-    cudaChannelFormatDesc channelDesc = cudaCreateChannelDesc(32, 0, 0, 0, cudaChannelFormatKindFloat);
-    cudaArray *cuArray;
-    checkCudaErrors(cudaMallocArray(&cuArray,&channelDesc,width,height));
-    checkCudaErrors(cudaMemcpyToArray(cuArray,0,0,hData,size,cudaMemcpyHostToDevice));
-
-    // Set texture parameters
-    tex.addressMode[0] = cudaAddressModeWrap;
-    tex.addressMode[1] = cudaAddressModeWrap;
-    tex.filterMode = cudaFilterModePoint;//cudaFilterModeLinear;
-    tex.normalized = true;// access with normalized texture coordinates
-
-    // Bind the array to the texture
-    checkCudaErrors(cudaBindTextureToArray(tex, cuArray, channelDesc));
-
-    checkCudaErrors(cudaDeviceSynchronize());
-    StopWatchInterface *timer = NULL;
-    sdkCreateTimer(&timer);
-    sdkStartTimer(&timer);
-
-    // Allocate mem for the result on host side
-    float *hOutputData = (float *) malloc(size);
-
-    // Execute the kernel
-    applyFilters<<<dimGrid, dimBlock, 0>>>(input_1, output, commutation_array, width, height);
-
-    // Check if kernel execution generated an error
-    getLastCudaError("Kernel execution failed");
-
-    checkCudaErrors(cudaDeviceSynchronize());
-    sdkStopTimer(&timer);
-    printf("Processing time: %f (ms)\n", sdkGetTimerValue(&timer));
-    printf("%.2f Mpixels/sec\n",(width *height / (sdkGetTimerValue(&timer) / 1000.0f)) / 1e6);
-    sdkDeleteTimer(&timer);
-
-    // copy result from device to host
-    checkCudaErrors(cudaMemcpy(hOutputData,output, size,cudaMemcpyDeviceToHost));
-
-    // Write result to file
-    char outputFilename[1024];
-    strcpy(outputFilename, imagePath);
-    strcpy(outputFilename + strlen(imagePath) - 4, "_out.pgm");
-    sdkSavePGM(outputFilename, hOutputData, width, height);
-    printf("Wrote '%s'\n", outputFilename);
-
-    checkCudaErrors(cudaFree(commutation_array));
-    checkCudaErrors(cudaFree(input_1));
-    checkCudaErrors(cudaFree(output));
-
-    checkCudaErrors(cudaFreeArray(cuArray));
-    free(rindex);
-    free(imagePath);
-    free(hOutputData);
-}
