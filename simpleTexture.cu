69754e33a2decd2785278308ef2061c2f9748343
Hygde <samuel.devals@outlook.fr>
2018-12-08 16:54:59 +0100

    reshape dispersion filter with events

--- simpleTexture.cu
+++ simpleTexture.cu
@@ -23,7 +23,7 @@ todo:
 //#include opencv
 #include <opencv2/opencv.hpp>
 
-__device__ void triArray(float *a, const int size){
+__device__ void triArray(unsigned char *a, const int size){
     float tmp;
     for(int i = 0; i < size; i ++){
         for(int j = 0; j < size-1; j++){
@@ -36,71 +36,93 @@ __device__ void triArray(float *a, const int size){
     }
 }
 
-__device__ void medianFilter(float*input, float *outputData,int width,int height){
+__device__ void medianFilter(unsigned char*input, unsigned char *outputData, int window, int width,int height){
     // calculate normalized texture coordinates
-    unsigned int x = blockIdx.x*blockDim.x + threadIdx.x;
-    unsigned int y = blockIdx.y*blockDim.y + threadIdx.y;
-    //to find the median -> tri à bulle + valeur du milieu
-    float values[9];
-    if(x > 0 && x < width-1 && y > 0 && y < height -1){
-        int index = 0;
-        for(int i = -1; i < 2; i++){
-            for(int j = -1; j < 2; j++){
-                values[index] = input[(y+j)*width+(x+i)];
-                index++;
-            }
-        }
-        triArray(values, 9);
-        outputData[y*width + x] = values[4];
-    }else outputData[y*width + x] =  input[y*width + x];
-}
-
-__device__ void sobelFilter(float *input, float*outputData, int width, int height){
-    unsigned int x = blockIdx.x*blockDim.x + threadIdx.x;
-    unsigned int y = blockIdx.y*blockDim.y + threadIdx.y;
-
-    if(x > 0 && x < width-1 && y > 0 && y < height -1){
-        char sobel_x[9] = {-1, 0, 1, -2, 0, 2, -1, 0, 1};
-        char sobel_y[9] = {-1, -2, -1, 0, 0, 0, 1, 2, 1};
-        float pixel_x = 0.0, pixel_y = 0.0;
-        int index = 0;
-        for(int i = -1; i < 2; i++){
-            for(int j = -1; j < 2; j++){
-                pixel_x += input[(y+j)*width + (x+i)] * sobel_x[index];
-                pixel_y += input[(y+j)*width + (x+i)] * sobel_y[index];
-                index++;
-            }
-        }
-        outputData[y*width + x] = sqrt((pixel_x*pixel_x)+(pixel_y*pixel_y)); 
-    }
-}
-
-__device__ void multiply(float*input_1, float*output, int width, int height){
-    unsigned int x = blockIdx.x*blockDim.x + threadIdx.x;
-    unsigned int y = blockIdx.y*blockDim.y + threadIdx.y;
-    output[y*width + x] *= input_1[y*width + x];
-}
-
-__device__ void dispersionFilter(unsigned char*input, unsigned char *outputData,char*commutation_array, int width, int height){
     unsigned int x = blockIdx.x*blockDim.x + threadIdx.x;
     unsigned int y = blockIdx.y*blockDim.y + threadIdx.y;
     unsigned int id = y * width + x;
 
-    if(x > 1 && y > 1 && x < width-2 && y < height-2){
-        //tex2D(tex, u+xstep*commutation_array[id], v+ystep*commutation_array[id+1]);
+    //to find the median -> tri à bulle + valeur du milieu
+    unsigned char b_values[9], g_values[9], r_values[9];//todo: window²
+    if(x > 0 && x < width-1 && y > 0 && y < height -1){
+        int index = 0;
+        for(int i = -1; i < 2; i++){
+            for(int j = -1; j < 2; j++){
+                b_values[index] = input[3 * ((y+j)*width+(x+i)) + 0];
+                g_values[index] = input[3 * ((y+j)*width+(x+i)) + 1];
+                r_values[index] = input[3 * ((y+j)*width+(x+i)) + 2];
+                index++;
+            }
+        }
+        triArray(b_values, 9);triArray(g_values, 9);triArray(r_values, 9);
+        outputData[3 * id + 0] = b_values[4];//apply median
+        outputData[3 * id + 1] = g_values[4];
+        outputData[3 * id + 2] = r_values[4];
+    }else{//copy
+        outputData[3 * id + 0] =  input[3 * id + 0];
+        outputData[3 * id + 1] =  input[3 * id + 0];
+        outputData[3 * id + 2] =  input[3 * id + 0];
+    }
+}
+
+__device__ void sobelFilter(unsigned char *input, unsigned char *outputData, int width, int height){
+    unsigned int x = blockIdx.x*blockDim.x + threadIdx.x;
+    unsigned int y = blockIdx.y*blockDim.y + threadIdx.y;
+    unsigned int id = y * width + x;
+
+    if(x > 0 && x < width-1 && y > 0 && y < height -1){
+        char sobel_x[9] = {-1, 0, 1, -2, 0, 2, -1, 0, 1};
+        char sobel_y[9] = {-1, -2, -1, 0, 0, 0, 1, 2, 1};
+        float b_pixel[2] = {0, 0}, g_pixel[2] = {0, 0}, r_pixel[2] = {0, 0};
+        int index = 0;
+        for(int i = -1; i < 2; i++){
+            for(int j = -1; j < 2; j++){
+                b_pixel[0] += input[3*((y+j)*width + (x+i))] * sobel_x[index];
+                b_pixel[1] += input[3*((y+j)*width + (x+i))] * sobel_y[index];
+                g_pixel[0] += input[3*((y+j)*width + (x+i))] * sobel_x[index];
+                g_pixel[1] += input[3*((y+j)*width + (x+i))] * sobel_y[index];
+                r_pixel[0] += input[3*((y+j)*width + (x+i))] * sobel_x[index];
+                r_pixel[1] += input[3*((y+j)*width + (x+i))] * sobel_y[index];
+                index++;
+            }
+        }
+        float b = sqrt((b_pixel[0]*b_pixel[0])+(b_pixel[1]*b_pixel[1]));
+        float g = sqrt((g_pixel[0]*g_pixel[0])+(g_pixel[1]*g_pixel[1]));
+        float r = sqrt((r_pixel[0]*r_pixel[0])+(r_pixel[1]*r_pixel[1]));
+
+        outputData[3 * id + 0] = sqrt((b_pixel[0]*b_pixel[0])+(b_pixel[1]*b_pixel[1]));//(b>255)?255:b;
+        outputData[3 * id + 1] = sqrt((g_pixel[0]*g_pixel[0])+(g_pixel[1]*g_pixel[1]));//(g>255)?255:g;
+        outputData[3 * id + 2] = sqrt((r_pixel[0]*r_pixel[0])+(r_pixel[1]*r_pixel[1]));//(r>255)?255:r;
+    }
+}
+
+__device__ void multiply(unsigned char*input_1, unsigned char*output, int width, int height){
+    unsigned int x = blockIdx.x*blockDim.x + threadIdx.x;
+    unsigned int y = blockIdx.y*blockDim.y + threadIdx.y;
+    unsigned int id = y * width + x;
+
+    output[3 * id + 0] *= input_1[3 * id + 0] / 255.0f;
+    output[3 * id + 1] *= input_1[3 * id + 1] / 255.0f;
+    output[3 * id + 2] *= input_1[3 * id + 2] / 255.0f;
+}
+
+__device__ void dispersionFilter(unsigned char*input, unsigned char *outputData,char*commutation_array, int window, int width, int height){
+    unsigned int x = blockIdx.x*blockDim.x + threadIdx.x;
+    unsigned int y = blockIdx.y*blockDim.y + threadIdx.y;
+    unsigned int id = y * width + x;
+
+    if(x > window/2 && y > window/2 && x < width-(window/2) && y < height-(window/2)){
         unsigned int index = 3 * ((y+commutation_array[id+1])*width + x + commutation_array[id]);
-        if(id == 2*width+15)printf("cuda print: %d %d %d\n", outputData[id+0], outputData[id+1], outputData[id+2]);
         outputData[3*id+0] = input[index+0];
         outputData[3*id+1] = input[index+1];
         outputData[3*id+2] = input[index+2];
-        if(id == 2*width+15)printf("cuda print: %d %d %d\n", outputData[3*id+0], outputData[3*id+1], outputData[3*id+2]);
     }else outputData[3*id] = input[3*id];
 }
 
-__global__ void applyFilters(unsigned char *input, unsigned char *outputData, char*commutation_array, int width, int height){
-    dispersionFilter(input, outputData,commutation_array, width, height);
-    //__syncthreads();
-    /*medianFilter(input, outputData, width, height);
+__global__ void applyFilters(unsigned char *input, unsigned char *outputData, char*commutation_array, int window,int width, int height){
+    dispersionFilter(input, outputData,commutation_array, window, width, height);
+    /*__syncthreads();
+    medianFilter(outputData, input, window, width, height);
     __syncthreads();
     sobelFilter(outputData, input, width, height);//result of sobel in input
     __syncthreads();
@@ -115,13 +137,33 @@ void sendDataToGpu(unsigned char* data, unsigned char*devData, unsigned char*dev
     checkCudaErrors(cudaMemcpy(devCom, com, (size+1)*sizeof(char), cudaMemcpyHostToDevice));
 }
 
+bool processEvent(bool&continuer, int&window){
+    bool result;
+    char carac = cv::waitKey(33);
+    switch(carac){
+        case 32:
+            continuer = false;
+        break;
+        case 82:
+            window += 2;
+            printf("window size = %dx%d \n",window, window);
+        break;
+        case 84:
+            window = (window > 3)?window - 2: window;
+            printf("window size = %dx%d \n",window, window);
+        break;
+        default:result = false;break;
+    }
+    return result;
+}
+
 ////////////////////////////////////////////////////////////////////////////////
 // Program main
 ////////////////////////////////////////////////////////////////////////////////
 int main(int argc, char **argv){
     findCudaDevice(argc, (const char **) argv);
     srand(time(NULL));
-    bool continuer = true;
+    bool continuer = true, config = true;
     int width = 0, height = 0, window = 5;
     cv::VideoCapture cam(0); cv::Mat img;
     unsigned char*data = NULL, *devData = NULL, *devBuffer = NULL;
@@ -136,23 +178,26 @@ int main(int argc, char **argv){
     checkCudaErrors(cudaMalloc((void**) &devCom, (width*height+1) * sizeof(char)));
 
     while(continuer){
-        getCommutationArray(com, width*height+1, window);
+        if(config){
+            config = false;
+            getCommutationArray(com, width*height+1, window);
+        }
         cam >> img; width = img.cols; height = img.rows; data = (unsigned char*)img.data;
         sendDataToGpu(data, devData, devBuffer, com, devCom, width*height);
         //begin main
-        
-        printf("before %d %d %d\n" , data[3*(2*width+15)], data[3*(2*width+15)+1], data[3*(2*width+15)+1]);
 
-        applyFilters<<<dimGrid, dimBlock, 0>>>(devBuffer, devData, devCom, width, height);
+        StopWatchInterface *timer = NULL;
+        sdkCreateTimer(&timer);
+        sdkStartTimer(&timer);
+        applyFilters<<<dimGrid, dimBlock, 0>>>(devBuffer, devData, devCom, window, width, height);
+        checkCudaErrors(cudaMemcpy(data, devData, 3*width*height*sizeof(char), cudaMemcpyDeviceToHost));
+        sdkStopTimer(&timer);
+        printf("Processing time: %f (ms)\r", sdkGetTimerValue(&timer));
 
         //end main
-        checkCudaErrors(cudaMemcpy(data, devData, 3*width*height*sizeof(char), cudaMemcpyDeviceToHost));
-        checkCudaErrors(cudaDeviceSynchronize());
-
-        printf("after %d %d %d\n\n" , data[3*(2*width+15)], data[3*(2*width+15)+1], data[3*(2*width+15)+1]);
 
         cv::imshow("test", img);
-        if(cv::waitKey(33) == ' ')continuer = false;
+        config = processEvent(continuer, window);
     }
 
     cam.release();
